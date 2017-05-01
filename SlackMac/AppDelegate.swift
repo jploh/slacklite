565ac4977b3c2fb7daa1a5516ec0ae63415fff1d
JP Loh <jploh@jp-mbp-core.local>
2017-05-01 17:30:22 +0800

    Initial Commit

---
+++ SlackMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SlackMac
+//
+//  Created by JP Loh on 01/05/2017.
+//  Copyright © 2017 JP Loh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
